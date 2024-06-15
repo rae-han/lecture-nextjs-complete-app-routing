7210b261ce3104cdc27bb3d1af8811d6c445f866
raehan <raehan900809@gmail.com>
2024-06-15 16:42:24 +0900

    feature: 프로젝트 기본 페이지 추가

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,20 @@
 import './globals.css';
+import Link from "next/link";
+import MainHeader from "@/components/main-header";
 
 export const metadata = {
   title: 'Next.js Page Routing & Rendering',
   description: 'Learn how to route to different pages.',
 }
-
 export default function RootLayout({ children }) {
  return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div id="page">
+          <MainHeader />
+          <main>{children}</main>
+        </div>
+      </body>
     </html>
   )
 }
---
+++ app/news/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Link from "next/link";
+import { DUMMY_NEWS } from '@/dummy-news';
+
+const NewsListPage = () => {
+  return (
+    <>
+      <h1>News Page</h1>
+      <ul className="news-list">
+        {DUMMY_NEWS.map((newsItem) => (
+          <li key={newsItem.id}>
+            <Link href={`/news/${newsItem.slug}`}>
+              <img
+                src={`/images/news/${newsItem.image}`}
+                alt={newsItem.title}
+              />
+              <span>{newsItem.title}</span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default NewsListPage;
